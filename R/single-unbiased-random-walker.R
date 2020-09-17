c031efd40c58b08c8fd155b769e6f7458b7def38
Travis Byrum <travis.tbyrum@gmail.com>
2020-09-17 01:22:33 -0400

    Update file paths

--- R/single-unbiased-random-walker.R
+++ R/single-unbiased-random-walker.R
@@ -1,29 +1,27 @@
-#' singleUnbaisedRandomWalker.R
-#'
 #' Implementation of voter model dynamics on a network in R.
 #'
 #' Simulate voter-model-style dynamics on a network.
 #' Nodes are randomly assigned a state in (-1,1) at each
 #' time step all nodes asynchronously update by choosing their new
-#' state uniformly from their neighbors. 
+#' state uniformly from their neighbors.
 #' Generates an N*L time series.
 #' The results dictionary also stores the time series as TS and ground truth adjacency matrix as ground_truth.
 #'
 #' @param inputMatrix the input (ground-truth) graph with N nodes. Must be valid square adjacency matrix.
 #' @param L the length of the desired time series.
-#' @param initialNode starting node for walk 
+#' @param initialNode starting node for walk
 #' @return results a list with TS matrix an N*L array of synthetic time series data.
 
 #' @export
 singleUnbaisedRandomWalker <- function(inputMatrix, L, initialNode=NULL) {
   # create return list
   results <- list()
-  
+
   # get adj matrix and set up vector of indices
   G = igraph::graph_from_adjacency_matrix(inputMatrix, weighted = TRUE)
   A = igraph::get.adjacency(G)
   N = unlist(DIM(igraph::V(G)))
-  
+
   # place walker at init location and walk
   if (is.null(initialNode)) {
     W = igraph::random_walk(G, sample(1:N, 1), steps=L)
@@ -31,16 +29,16 @@ singleUnbaisedRandomWalker <- function(inputMatrix, L, initialNode=NULL) {
   else {
     W = igraph::random_walk(G, initialNode, steps=L)
   }
-  
+
   # store ground truth
   results[["node_index_sequence"]] = W
-  
+
   # turn into binary-valued
   TS = matrix(rep(0, N * L), nrow = N)
   for (i in 1:length(W)) {
     TS[(W[i]), i ] = 1
   }
-  
+
   # return results
   results[["TS"]] = TS
   results[["ground_truth"]] = input
--- R/voter.R
+++ R/voter.R
@@ -1,17 +1,15 @@
-#' voter.R
-#'
 #' Implementation of voter model dynamics on a network in R.
 #'
 #' Simulate voter-model-style dynamics on a network.
 #' Nodes are randomly assigned a state in (-1,1) at each
 #' time step all nodes asynchronously update by choosing their new
-#' state uniformly from their neighbors. 
+#' state uniformly from their neighbors.
 #' Generates an N*L time series.
 #' The results dictionary also stores the time series as TS and ground truth adjacency matrix as ground_truth.
 #'
 #' @param inputMatrix the input (ground-truth) graph with `N` nodes. Must be valid square adjacency matrix.
 #' @param L the length of the desired time series.
-#' @param noise if noise is present, with this probability a node's state will be randomly redrawn from (-1,1) \cr 
+#' @param noise if noise is present, with this probability a node's state will be randomly redrawn from (-1,1) \cr
 #' independent of its neighbors' states. If 'automatic', set noise to 1/N.
 #' @return results a list with TS matrix an N*L array of synthetic time series data.
 
@@ -28,26 +26,26 @@ DIM <- function( ... ){
 voter <- function(inputMatrix, L, noise=NULL) {
   # create return list
   results <- list()
-  
+
   # get adj matrix
   G = igraph::graph_from_adjacency_matrix(inputMatrix, weighted = TRUE)
   # get num of nodes in igraph obj
-  N = unlist(DIM(igraph::V(G))) 
+  N = unlist(DIM(igraph::V(G)))
 
-  
+
   # noise input validation
   if (is.null(noise)) {
     noise <- 0
   }
   if (identical(noise, "automatic") || identical(noise, "auto")) {
     noise <- 1 / N
-  } 
+  }
   if (!is.numeric(noise)) {
     stop("noise must be a number, 'automatic', or NULL")
   }
-  
+
   # get adj mat
-  transitions = igraph::get.adjacency(G)  
+  transitions = igraph::get.adjacency(G)
   # sum cols of adjmat for n columns
   colSums = unname(tapply(igraph::E(G)$weight, (seq_along(igraph::E(G)$weight)-1) %/% ncol(transitions), sum))
   # divide each vertex's weight by sum of edge weights for that vertex
@@ -60,7 +58,7 @@ voter <- function(inputMatrix, L, noise=NULL) {
       colSumIter = colSumIter + 1
     }
   }
-  
+
   # generate time series
   TS = matrix(rep(0, N * L), nrow = N)
   randNum = runif(N) # random uniform distribution
@@ -71,13 +69,13 @@ voter <- function(inputMatrix, L, noise=NULL) {
       TS[i,1] = -1
     }
   }
-  
+
   # indices to help with random sampling
   indices = 1:N
-  
+
   # iterate through time series data and use transition graph weights as sum
   for (t in 2:L) {
-    
+
     TS[,t] = TS[,t-1]
     for (i in indices) {
       TS[i,t] <- sample(c(TS[,t]), 1, replace = FALSE, prob = c(G[i]))
@@ -91,11 +89,11 @@ voter <- function(inputMatrix, L, noise=NULL) {
         }
       }
     }
-    
+
   }
   results[["ground_truth"]] = inputMatrix
   results[["TS"]] = TS
-  
+
   return(results)
 }
 
